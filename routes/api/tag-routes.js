2fc334ad480e7a5ad2406e3da159b1cb8e7ffd23
Michael Li <thisislimichael@gmail.com>
2021-12-30 19:28:53 -0800

    added routes for the models

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,33 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model: Product}, {model: ProductTag}]
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include: [{model: Product}, {model: ProductTag}]
+  })
+    .then(tagData => {
+      if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+      }
+      res.json(tagData);
+    })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
